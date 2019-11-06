34ec562803a8335be1027c00fb87359aaeae17ab
Benjamin Anderson <heartbeast.studios@gmail.com>
2019-11-06 16:07:55 -0700

    Platform Character + Review

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,19 +3,50 @@ extends KinematicBody2D
 export (int) var ACCELERATION = 512
 export (int) var MAX_SPEED = 64
 export (float) var FRICTION = 0.25
+export (int) var GRAVITY = 200
+export (int) var JUMP_FORCE = 128
+export (int) var MAX_SLOPE_ANGLE = 46
 
 var motion = Vector2.ZERO
 
 func _physics_process(delta):
+	var input_vector = get_input_vector()
+	apply_horizontal_force(input_vector, delta)
+	apply_friction(input_vector)
+	jump_check()
+	apply_gravity(delta)
+	move()
+
+func get_input_vector():
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
-	if input_vector != Vector2.ZERO:
-		motion += input_vector * ACCELERATION * delta
-		motion = motion.clamped(MAX_SPEED)
+	return input_vector
+
+func apply_horizontal_force(input_vector, delta):
+	if input_vector.x != 0:
+		motion.x += input_vector.x * ACCELERATION * delta
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+
+func apply_friction(input_vector):
+	if input_vector.x == 0 and is_on_floor():
+		motion.x = lerp(motion.x, 0, FRICTION)
+
+func jump_check():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -JUMP_FORCE
 	else:
-		motion = motion.linear_interpolate(Vector2.ZERO, FRICTION)
-	
-	
-	motion = move_and_slide(motion)
+		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2;
+
+func apply_gravity(delta):
+#	if not is_on_floor():
+		motion.y += GRAVITY * delta
+		motion.y = min(motion.y, JUMP_FORCE)
+
+func move():
+	motion = move_and_slide(motion, Vector2.UP)
+
+
+
+
