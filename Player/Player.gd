33610364eb5f5e3e8822d731c88d58b2e26cc6d2
Benjamin Anderson <heartbeast.studios@gmail.com>
2019-11-07 16:44:56 -0700

    Fixing Slope Movement + Review

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,11 +9,13 @@ export (int) var MAX_SLOPE_ANGLE = 46
 
 var motion = Vector2.ZERO
 var snap_vector = Vector2.ZERO
+var just_jumped = false
 
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
 
 func _physics_process(delta):
+	just_jumped = false
 	var input_vector = get_input_vector()
 	apply_horizontal_force(input_vector, delta)
 	apply_friction(input_vector)
@@ -45,6 +47,7 @@ func jump_check():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
+			just_jumped = true
 			snap_vector = Vector2.ZERO
 	else:
 		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
@@ -66,8 +69,24 @@ func update_animations(input_vector):
 		spriteAnimator.play("Jump")
 
 func move():
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_motion = motion
+	var last_position = position
+	
 	motion = move_and_slide_with_snap(motion, snap_vector*4, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
-
+	# Landing
+	if was_in_air and is_on_floor():
+		motion.x = last_motion.x
+	
+	# Just left ground
+	if was_on_floor and not is_on_floor() and not just_jumped:
+		motion.y = 0
+		position.y = last_position.y
+	
+	# Prevent Sliding (hack)
+	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 1:
+		position.x = last_position.x
 
 
 
