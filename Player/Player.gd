a60043a22f8f3faca96b1c361f22380f5f7a338c
Benjamin Anderson <heartbeast.studios@gmail.com>
2019-11-07 16:30:58 -0700

    Slopes + Snapping

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,7 @@ export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
 
 var motion = Vector2.ZERO
+var snap_vector = Vector2.ZERO
 
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
@@ -16,6 +17,7 @@ func _physics_process(delta):
 	var input_vector = get_input_vector()
 	apply_horizontal_force(input_vector, delta)
 	apply_friction(input_vector)
+	update_snap_vector()
 	jump_check()
 	apply_gravity(delta)
 	update_animations(input_vector)
@@ -35,16 +37,21 @@ func apply_friction(input_vector):
 	if input_vector.x == 0 and is_on_floor():
 		motion.x = lerp(motion.x, 0, FRICTION)
 
+func update_snap_vector():
+	if is_on_floor():
+		snap_vector = Vector2.DOWN
+
 func jump_check():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
+			snap_vector = Vector2.ZERO
 	else:
 		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
 			motion.y = -JUMP_FORCE/2;
 
 func apply_gravity(delta):
-#	if not is_on_floor():
+	if not is_on_floor():
 		motion.y += GRAVITY * delta
 		motion.y = min(motion.y, JUMP_FORCE)
 
@@ -59,7 +66,7 @@ func update_animations(input_vector):
 		spriteAnimator.play("Jump")
 
 func move():
-	motion = move_and_slide(motion, Vector2.UP)
+	motion = move_and_slide_with_snap(motion, snap_vector*4, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
 
 
 
