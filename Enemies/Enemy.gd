992bf716a9fb91dc5e1b08b1c5d40f034a262bd7
Benjamin Anderson <heartbeast.studios@gmail.com>
2019-11-27 16:27:26 -0700

    Walking Enemy + Raycasting

---
+++ Enemies/Enemy.gd
@@ -0,0 +1,4 @@
+extends KinematicBody2D
+
+export(int) var MAX_SPEED = 15
+var motion = Vector2.ZERO
---
+++ Enemies/WalkingEnemy.gd
@@ -0,0 +1,31 @@
+extends "res://Enemies/Enemy.gd"
+
+enum DIRECTION {LEFT = -1, RIGHT = 1}
+
+export(DIRECTION) var WALKING_DIRECTION
+
+var state
+
+onready var sprite = $Sprite
+onready var floorLeft = $FloorLeft
+onready var floorRight = $FloorRight
+onready var wallLeft = $WallLeft
+onready var wallRight = $WallRight
+
+func _ready():
+	state = WALKING_DIRECTION
+
+func _physics_process(delta):
+	match state:
+		DIRECTION.RIGHT:
+			motion.x = MAX_SPEED
+			if not floorRight.is_colliding() or wallRight.is_colliding():
+				state = DIRECTION.LEFT
+		
+		DIRECTION.LEFT:
+			motion.x = -MAX_SPEED
+			if not floorLeft.is_colliding() or wallLeft.is_colliding():
+				state = DIRECTION.RIGHT
+	
+	sprite.scale.x = sign(motion.x)
+	motion = move_and_slide_with_snap(motion, Vector2.DOWN * 4, Vector2.UP, true, 4, deg2rad(46))
