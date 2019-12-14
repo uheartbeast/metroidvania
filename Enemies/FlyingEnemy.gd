329af6aca6c148c3d63911df166cd2fb7e28f661
Benjamin Anderson <heartbeast.studios@gmail.com>
2019-12-13 20:52:35 -0700

    Flying Enemy + Review

---
+++ Enemies/FlyingEnemy.gd
@@ -0,0 +1,19 @@
+extends "res://Enemies/Enemy.gd"
+
+export(int) var ACCELERATION = 100
+
+var MainInstances = ResourceLoader.MainInstances
+
+onready var sprite = $Sprite
+
+func _physics_process(delta):
+	var player = MainInstances.Player
+	if player != null:
+		chase_player(player, delta)
+
+func chase_player(player, delta):
+	var direction = (player.global_position - global_position).normalized()
+	motion += direction * ACCELERATION * delta
+	motion = motion.clamped(MAX_SPEED)
+	sprite.flip_h = global_position < player.global_position
+	motion = move_and_slide(motion)
