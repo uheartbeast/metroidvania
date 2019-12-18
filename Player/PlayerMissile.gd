4ccc6d185c32b086dd7a917a4064ef960e8c70bb
Benjamin Anderson <heartbeast.studios@gmail.com>
2019-12-18 10:15:58 -0700

    Brick + Review

---
+++ Player/PlayerMissile.gd
@@ -0,0 +1,12 @@
+extends "res://Player/Projectile.gd"
+
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
+const BRICK_LAYER_BIT = 4
+
+func _on_Hitbox_body_entered(body):
+	if body.get_collision_layer_bit(BRICK_LAYER_BIT):
+		body.queue_free()
+		Utils.instance_scene_on_main(EnemyDeathEffect, body.global_position + Vector2(8, 8))
+	._on_Hitbox_body_entered(body)
+	
