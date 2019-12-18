f39a90c8c4dc9107e9f9c92aa17411bc802b8c5e
Benjamin Anderson <heartbeast.studios@gmail.com>
2019-12-18 09:47:41 -0700

    Missiles + Scale Glitch

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,7 @@ const DustEffect = preload("res://Effects/DustEffect.tscn")
 const JumpEffect = preload("res://Effects/JumpEffect.tscn")
 const WallDustEffect = preload("res://Effects/WallDustEffect.tscn")
 const PlayerBullet = preload("res://Player/PlayerBullet.tscn")
+const PlayerMissile = preload("res://Player/PlayerMissile.tscn")
 
 var PlayerStats = ResourceLoader.PlayerStats
 var MainInstances = ResourceLoader.MainInstances
@@ -17,6 +18,7 @@ export (int) var MAX_WALL_SLIDE_SPEED = 128
 export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
 export (int) var BULLET_SPEED = 250
+export (int) var MISSILE_BULLET_SPEED = 150
 
 enum {
 	MOVE,
@@ -76,12 +78,23 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("fire") and fireBulletTimer.time_left == 0:
 		fire_bullet()
+	
+	if Input.is_action_pressed("fire_missile") and fireBulletTimer.time_left == 0:
+		fire_missile()
 
 func fire_bullet():
 	var bullet = Utils.instance_scene_on_main(PlayerBullet, muzzle.global_position)
 	bullet.velocity = Vector2.RIGHT.rotated(gun.rotation) * BULLET_SPEED
 	bullet.velocity.x *= sprite.scale.x
 	bullet.rotation = bullet.velocity.angle()
+	fireBulletTimer.start()
+
+func fire_missile():
+	var missile = Utils.instance_scene_on_main(PlayerMissile, muzzle.global_position)
+	missile.velocity = Vector2.RIGHT.rotated(gun.rotation) * MISSILE_BULLET_SPEED
+	missile.velocity.x *= sprite.scale.x
+	motion -= missile.velocity * 0.25
+	missile.rotation = missile.velocity.angle()
 	fireBulletTimer.start()
 
 func create_dust_effect():
@@ -131,7 +144,10 @@ func apply_gravity(delta):
 		motion.y = min(motion.y, JUMP_FORCE)
 
 func update_animations(input_vector):
-	sprite.scale.x = sign(get_local_mouse_position().x)
+	var facing = sign(get_local_mouse_position().x)
+	if facing != 0:
+		sprite.scale.x = facing
+	
 	if input_vector.x != 0:
 		spriteAnimator.play("Run")
 		spriteAnimator.playback_speed = input_vector.x * sprite.scale.x
